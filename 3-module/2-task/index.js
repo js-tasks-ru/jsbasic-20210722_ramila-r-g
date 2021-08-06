93d6c181a6bfa4d1d82993de4e146a24d0d6b141
Ramila G <ramila.r.g@gmail.com>
2021-08-06 14:54:03 +0300

    решила задачи 3 модуля

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,5 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+  let filteredArray = arr.filter((element) => {return a <= element && element <= b} 
+);
+  return filteredArray;
 }
--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
 function camelize(str) {
-  // ваш код...
+  let wordArray = str.split('');
+  for (let i = 0; i < wordArray.length; i++){
+    if (wordArray[i - 1] === '-') {
+     wordArray[i] = wordArray[i].toUpperCase();
+      wordArray[i - 1];
+    }
+  }
+  let camelString = wordArray.join('').split('-').join('');
+  return camelString;
 }
