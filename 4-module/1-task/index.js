100331737f0feae1adf73bd64d58663472de099f
ramila-g <ramila.r.g@gmail.com>
2021-08-14 21:04:16 +0300

    решила задачи 4 урока

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,14 @@
 function makeFriendsList(friends) {
-  // ваш код...
-}
+  let uList = document.createElement('ul');
+  let users = []
+  
+  friends.forEach((friend) => {
+    let newLi = document.createElement('li');
+    users.push(friend.firstName + ' ' + friend.lastName)    
+    uList.append(newLi);
+    newLi.innerHTML = friend.firstName + ' ' + friend.lastName
+  }
+  );
+
+  return uList;
+};
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,24 @@
 function highlight(table) {
-  // ваш код...
+  let tableRows = table.querySelectorAll('tr');
+  
+tableRows.forEach ((row) => {
+  let itsTds = row.querySelectorAll('td');
+  
+  if (itsTds[2].innerHTML === 'm'){
+    row.classList.add('male')
+  } else if (itsTds[2].innerHTML === 'f') {row.classList.add('female')}
+  
+  if (itsTds[1].innerHTML < 18) {
+    row.style.textDecoration = 'line-through';
+  }
+  
+  if (itsTds[3].dataset.available === 'true') {
+    row.classList.add('available')
+  } else if (itsTds[3].dataset.available === 'false') {
+    row.classList.add('unavailable')
+  } else {
+    row.setAttribute("hidden", true);
+  }
+  }); 
+  return table;
 }
